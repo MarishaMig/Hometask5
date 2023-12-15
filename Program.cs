fba8c4d49a3b697d9a8a3a371524180ccddbc308
Marina Migurskaya <marina.migurskaya@gmail.com>
2023-12-15 18:11:56 +0300

    2 task

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿/* 1. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
 и возвращает значение этого элемента или же указание, что такого элемента нет */
 
-
+/*
 Console.WriteLine("Введите позицию элемента:");
 Console.Write("строка - ");
 int line=Convert.ToInt32(Console.ReadLine());
@@ -48,18 +48,52 @@ CreateArray();
 PrintArray();
 Console.WriteLine();
 FindElementArray(line,column);
-
-
-
-
-
-
-
-
-
-
+*/
 
 /* 2. Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива. */
+
+int[,] array = new int[5,5];
+void CreateArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeArray()
+{    
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        int temp=array[0,i];
+        array[0,i]=array[array.GetLength(0)-1,i];
+        array[array.GetLength(0)-1,i]=temp;
+    }
+}
+
+CreateArray();
+PrintArray();
+Console.WriteLine("  -------   ");
+ChangeArray();
+PrintArray();
+
+
+
 
 
 
